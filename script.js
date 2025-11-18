4373aa2d7fa7ee1689bf0e0817318f5d0f9e6391
spardhivan-sys <spardhivan@gmail.com>
2025-11-18 15:38:41 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculate() {
+    let result = document.getElementById('result').value;
+    try {
+        document.getElementById('result').value = eval(result);
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
